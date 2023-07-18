04ac11df7908a7fc5a80352dbfcf83e199edf8f1
AbdirahmanDirie <cabdirahmandirie@gmail.com>
2023-07-18 10:39:48 +0300

    first installization project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>TS Amazon</header>
+      <main>
+        <footer>All right reserved</footer>
+      </main>
+      
+    </div>
+  )
+}
+
+export default App
